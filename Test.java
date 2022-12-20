7b2d1fb07eda55d22acef45092ef9ec3f2fbc10d
The-GuardianARY <93256702+The-GuardianARY@users.noreply.github.com>
2022-12-20 12:21:30 +0530

    Testing ground for codes

--- Test.java
+++ Test.java
@@ -5,22 +5,6 @@ static Scanner sc=new Scanner(System.in);
 
 public static void main(String args[])
 {
-    System.out.println("You are Bowling");
-    int v1=0;
-    int u1=0;
-    int r1=0;
-    int CompBatting=0;
-    while(r1==0)
-    {
-        v1=(int)(Math.random()*10);
-        System.out.print("Your Turn : ");
-        u1=sc.nextInt();
-        System.out.println("Computer's Number ="+v1);
-        CompBatting=CompBatting+u1;
-        r1=v1==u1?1:0;
-        if(v1==u1)
-        System.out.println("Computer is OUT");
 
 }    
-}
 }
